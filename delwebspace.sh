5ba5a770d963c1dc9e9af040b71e78bdd0b61180
Manuel Zarat <manuel@zarat.ml>
2025-05-31 04:28:40 +0200

    Update delwebspace.sh

--- delwebspace.sh
+++ delwebspace.sh
@@ -3,16 +3,17 @@
 # Kundenname
 read -p "Bitte gib einen Kunden-namen ein: " kunde
 
-echo "Entferne Container"
-sudo docker stop $kunde
-sudo docker rm $kunde
+echo "Stoppe Container"
+sudo docker stop $kunde > /dev/null
 
-echo "Entferne NginX VHost Konfiguration"
+echo "Entferne Container"
+sudo docker rm $kunde > /dev/null
+
+echo "Entferne vHost Konfiguration"
 sudo rm "/etc/nginx/sites-available/$kunde"
 sudo rm "/etc/nginx/sites-enabled/$kunde"
 
 echo "Lade NginX neu"
-# Nginx-Konfiguration testen und neu laden
 sudo nginx -t && sudo systemctl reload nginx
 
 echo "VHost $kunde wurde entfernt."
