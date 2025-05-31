e400ccd37dee2ad97914eced29fc8e9a094fe748
Manuel Zarat <manuel@zarat.ml>
2025-05-31 04:28:11 +0200

    Update mkwebspace.sh

--- mkwebspace.sh
+++ mkwebspace.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-read -p "Kunden Name: " kunde
-read -p "Kunden Port: " port
+read -p "Name: " kunde
+read -p "externer SSH-Port: " port
+read -p "Username: " username
+
+password=$username!
 
 # Name des Docker-Containers
 CONTAINER_NAME=$kunde
 SERVER_NAME="$kunde.zarat.cloudns.nz"
 
-sudo docker run -dit --name $kunde -p $port:22 ubuntu-template
+
+echo "Erstelle Docker Container..."
+if sudo docker run -dit --name "$kunde" --hostname "$kunde.zarat.cloudns.nz" -p "$port":22 ubuntu-template > /dev/null 2>&1; then
+    echo " Container erfolgreich erstellt."
+else
+    echo " Fehler beim Erstellen des Containers."
+fi
+
+#sudo docker exec "$kunde" bash -c "sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+sudo docker exec "$kunde" bash -c "useradd -m -s /bin/bash $username && echo '$username:$password' | chpasswd"
+sudo docker exec "$kunde" bash -c "usermod -aG sudo $username"
+echo " Benutzer '$username' mit Passwort '$password' wurde erstellt. Login via SSH ist jetzt möglich."
+
 
 # Container-IP ermitteln
 CONTAINER_IP=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONTAINER_NAME")
@@ -17,6 +32,7 @@ if [ -z "$CONTAINER_IP" ]; then
   exit 1
 fi
 
+echo "Erstelle vHost Konfiguration"
 # vHost-Datei erzeugen
 cat <<EOF | sudo tee "/etc/nginx/sites-available/$kunde" > /dev/null
 server {
@@ -32,10 +48,12 @@ server {
 }
 EOF
 
+echo "Aktiviere vHost"
 # Symlink setzen
 sudo ln -s "/etc/nginx/sites-available/$kunde" "/etc/nginx/sites-enabled/$kunde"
 
+echo "Lade NginX neu"
 # Nginx Konfiguration testen und neuladen
 sudo nginx -t && sudo systemctl reload nginx
 
-echo "vHost für kundeE wurde erstellt  $CONTAINER_IP)"
+echo "vHost wurde erstellt ($CONTAINER_IP)"
